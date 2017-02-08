2ff73b677bcc48429ec9fa46d0b33839e35f93bc
patrykxk <patrix215@gmail.com>
2017-02-08 12:02:53 +0100

    gitignore and part of special cards checking" x xczxczx "

--- src/app/RoomThread.java
+++ src/app/RoomThread.java
@@ -19,6 +19,7 @@ class RoomThread extends Thread {
 	private int whoseTurn = 0;
 	private DataFromServer dataFromServer;
 	boolean isGameStarted = false;
+	int noOfPenaltyCards = 0;
 	
 	public boolean getIsGameStarted(){
 		return isGameStarted;
@@ -63,58 +64,76 @@ class RoomThread extends Thread {
 		return false;
 	}
 
-
+	
 	public void run() {
 		while(true){
-			Object readObject = null;
-			try {
-				System.out.println("Czytam------------------------------------");
-				readObject = clients.get(whoseTurn).getObjectInputStream().readObject();
-				
-			} catch (ClassNotFoundException | IOException e) {
-				e.printStackTrace();
-			}
 			
-			DataFromClient dataFromClient = (DataFromClient) readObject;
+			DataFromClient dataFromClient = readDataFromClient();
 
 			switch(dataFromClient.getPacketId()){
-				case 1:
+				case 1://Client put cards on Table
 					for(Card card : dataFromClient.getClientCards()){
 						cardsOnTable.add(card);
 					}
-					System.out.println("WhoseTurn: " + whoseTurn);
+					System.out.println("Po³o¿one karty Gracza " + whoseTurn);
+					System.out.println(dataFromClient.getClientCards().get(0).getCardValue() +" "+ dataFromClient.getClientCards().get(0).getSuit() );
 					whoseTurn = (whoseTurn +1)%(clients.size());
 					System.out.println("WhoseTurn2: " + whoseTurn);
-					DataFromServer dataFromServer = new DataFromServer(4, dataFromClient.getClientCards(), whoseTurn);
-					for(Client client : clients){
-						sendPacket(client,dataFromServer);
+					
+					if(!checkForSpecialCard(dataFromClient.getClientCards())){
+						DataFromServer dataFromServer = new DataFromServer(4, dataFromClient.getClientCards(), whoseTurn);
+						for(Client client : clients){
+							sendPacket(client,dataFromServer);
+						}
 					}
 					break;
-				case 2:
-					System.out.println("klient klikn¹³ koniec Ruchu");
-					System.out.println("WhoseTurn: " + whoseTurn);
+				case 2: //end Turn packet
 					int nextTurn = (whoseTurn +1)%(clients.size());
-					System.out.println("nextTurn: " + nextTurn);
 					dataFromServer = new DataFromServer(2, nextTurn);
-					System.out.println("ruch gracza: " + nextTurn);
 					sendPacket(clients.get(whoseTurn), dataFromServer);
 					whoseTurn = nextTurn;
 					sendPacket(clients.get(whoseTurn), dataFromServer);
 					break;
-				case 3:
+				case 3: //take card packet
 					ArrayList<Card> newClientCards = new ArrayList();
+					for(int i=0;i<5;i++){
 					newClientCards.add(cards.pop());
+					}
 					dataFromServer = new DataFromServer(3, newClientCards);
 					sendPacket(clients.get(whoseTurn), dataFromServer);	
 					System.out.println("WhoseTurn: " + whoseTurn);
 					break;
-
+				case 4: //take panalty cards
+					ArrayList<Card> penaltyCards = new ArrayList();
+ 					for(int i=0; i<noOfPenaltyCards;i++){
+ 						penaltyCards.add(cards.pop());
+ 					}
+ 					dataFromServer = new DataFromServer(8, penaltyCards, (whoseTurn +1)%(clients.size()));
+ 					noOfPenaltyCards=0;
+ 					sendPacket(clients.get(whoseTurn), dataFromServer);
+ 					whoseTurn = (whoseTurn +1)%(clients.size());
+ 					dataFromServer = new DataFromServer(4, whoseTurn);
+ 					sendPacket(clients.get(whoseTurn), dataFromServer);
+ 					break;
 			}
 			if(clients.size()<1){
 				break;
 			}
 	  }
-	  
+	}
+
+	private DataFromClient readDataFromClient() {
+		Object readObject = null;
+		try {
+			System.out.println("Czytam------------------------------------");
+			readObject = clients.get(whoseTurn).getObjectInputStream().readObject();
+			
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		DataFromClient dataFromClient = (DataFromClient) readObject;
+		return dataFromClient;
 	}
 	
 	
@@ -128,17 +147,32 @@ class RoomThread extends Thread {
 		}
 	}
 	
-	private void checkForSpecialCard(ArrayList<Card> clientCards) {
+	private boolean checkForSpecialCard(ArrayList<Card> clientCards) {
 		Card card = clientCards.get(clientCards.size()-1);
-		
+		boolean flag = false;
+		DataFromServer dataFromServer;
 		switch(card.getCardValue()){
 			case TWO:
+				noOfPenaltyCards = 2*clientCards.size();
+				dataFromServer = new DataFromServer(5,clientCards, whoseTurn);
+				sendPacket(clients.get(whoseTurn), dataFromServer);
+				flag = true;
 				break;
 			case THREE:
+				noOfPenaltyCards = 3*clientCards.size();
+				dataFromServer = new DataFromServer(6,clientCards, whoseTurn);
+				sendPacket(clients.get(whoseTurn), dataFromServer);
+				flag = true;
 				break;
-		
-		
+			case FOUR:
+				noOfPenaltyCards = 3*clientCards.size();
+				dataFromServer = new DataFromServer(7,clientCards, whoseTurn);
+				sendPacket(clients.get(whoseTurn), dataFromServer);
+				flag = true;
+				break;
+
 		}
+		return flag;
 		
 	}
 
